59aaf40e3db1974ed74921e7d78497adac489eef
88michalh <133773112+88michalh@users.noreply.github.com>
2023-05-21 15:08:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ try:
   else:
     #back_from_function = get_frutyvice_data(fruit_choise)
     #streamlit.dataframe(back_from_function)
-     streamlit.text(fruit_choise)
+     streamlit.text("hi!)
 
 except URLError as e:
   streamlit.error()
